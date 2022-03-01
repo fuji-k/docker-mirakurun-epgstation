caaa8e593de619afb1917cfc5fe62f29bc4b6956
thik98 <wizard.taka+github@gmail.com>
2022-03-01 12:18:05 +0900

    JLS Logo Install 方法変更

--- epgstation/config/enc.sh
+++ epgstation/config/enc.sh
@@ -14,7 +14,7 @@ FFMPEG="/usr/local/bin/ffmpeg"
 AVSBASEPATH="/root/JoinLogoScpTrialSetLinux/modules/join_logo_scp_trial/result/"
 $ARIBTS2ASS -o before.ass temp.ts
 export HOME="/root"
-$JLSE -i temp.ts -f true -e true -t cutcm_logo -o " -c:v h264_omx -vf yadif,scale=-2:720 -b:v 4M -aspect 16:9 -r 30000/1001 -c:a aac -ab 192K -ac 2 -bsf:a aac_adtstoasc"
+$JLSE -i temp.ts -f true -e true -t cutcm_logo -o " -c:v h264_omx -vf yadif,scale=-2:720 -b:v 8M -aspect 16:9 -r 30000/1001 -c:a aac -ab 192K -ac 2 -bsf:a aac_adtstoasc"
 cp $AVSBASEPATH/temp/in_cutcm.avs .
 if [ -e "before.ass" ];then
     $CUTASS -o "temp.ass" -t in_cutcm.avs "before.ass"
